2e108bdc9e7bff8c8d5549a11eaae2a4ae8d7f79
sanit0x79 <sanitycs@proton.me>
2024-12-09 13:12:41 +0100

    made sure I don't forget how to change the modes

--- code/main.c
+++ code/main.c
@@ -10,7 +10,7 @@ void setup() {
   WiFi.mode(WIFI_MODE_AP);
   WiFi.softAP(ssid, password);
 
-  WiFi.softAPsetProtocol(WIFI_PROTOCOL_11B);   
+  WiFi.softAPsetProtocol(WIFI_PROTOCOL_11B);  // change to 11G or 11N for different Wi-Fi modes 
   Serial.print("Access Point IP Address: ");
   Serial.println(WiFi.softAPIP());
 }
